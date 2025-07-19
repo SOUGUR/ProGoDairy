9a0410e867de4f9683fae548615516eecdfca0aa
sourabh <sourabhgurav23@gmail.com>
2025-07-19 23:58:48 +0530

    Deleted old migrates and added new models and migrations files

--- distribution/models.py
+++ distribution/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from plants.models import Tester
+from suppliers.models import Supplier
 
 class Route(models.Model):
     """
@@ -8,14 +10,14 @@ class Route(models.Model):
     """
     name = models.CharField(max_length=100, unique=True)
     tester = models.ForeignKey(
-        'Tester',
+        Tester,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
         related_name='routes'
     )
-    suppliers = models.ManyToManyField('Supplier', related_name='routes')
-    
+    suppliers = models.ManyToManyField(Supplier, related_name='routes')
+
     def __str__(self):
         return self.name
 
@@ -25,11 +27,11 @@ class Distributor(models.Model):
     Entity that owns/operates trucks and is responsible for pickup via a specific route.
     Each distributor can be assigned to one route.
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     address = models.TextField()
-    license_number = models.CharField(max_length=50, unique=True)
-    vehicle_name = models.CharField(max_length=100)
-    vehicle_id = models.CharField(max_length=50, unique=True)
+    license_number = models.CharField(max_length=100, null=True, blank=True)
+    vehicle_name = models.CharField(max_length=100, null=True, blank=True)
+    vehicle_id = models.CharField(max_length=50, null=True, blank=True)
     route = models.ForeignKey(
         Route,
         on_delete=models.SET_NULL,
