e5bd86b30b4945d941a0c513686fdc7d07c167ad
sourabh <sourabhgurav23@gmail.com>
2025-08-18 00:44:05 +0530

    added error page and Login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import user_access
+from .views import user_access, login
 app_name = "accounts"
 
 urlpatterns = [
     path('user-access/', user_access, name='user_access'),
+    path('login/', login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login as auth_login
 
 def user_access(request):
     return render(request, "accounts/rights_access.html")
+
+
+
+def login(request):
+    if request.method == 'POST':
+        print("POST data:", request.POST)
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print("====="*34)
+        print(username, password)
+        print("====="*34)
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth_login(request, user)
+            return redirect('supplier_list') 
+        else:
+            return render(request, "accounts/error_page.html", {'error': 'Invalid credentials'})
+        
+    return render(request, "homePage.html")
+
 
