6b40f09549bc946cd693cc122de1283de49dbf36
sourabh <sourabhgurav23@gmail.com>
2025-11-13 22:40:41 +0530

    fixed dashboard and UI

--- static/chatbot/js/chatbot.js
+++ static/chatbot/js/chatbot.js
@@ -29,7 +29,7 @@ async function sendMessage() {
   input.value = '';
 
   try {
-    const response = await fetch("accounts/api/chat/", {  
+    const response = await fetch("/accounts/api/chat/", {  
       method: "POST",
       headers: {
         "Content-Type": "application/json",
